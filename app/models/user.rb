bbf425b4799c6336386284d1f68c0ff575587f61
Hitesh <hiteshgurjar786@gmail.com>
2015-08-24 11:03:52 +0530

    asset precompile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-  # validates :mobile_number, :numericality => {:only_integer => true}
   validates_uniqueness_of :username
-  # validates :mobile_number, presence: true
   has_one :profile, :dependent => :destroy
   
   def fullname
